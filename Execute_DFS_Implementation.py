a902d3c28a9ea01862aac68030056616a7f1da1d
rickydodd-deakin <80746151+rickydodd-deakin@users.noreply.github.com>
2021-04-19 17:39:19 +1000

    Create Execute_DFS_Implementation.py
    Executed DFS on the problem. Not recommended.

---
+++ Execute_DFS_Implementation.py
@@ -0,0 +1,9 @@
+'''
+    * Executes depth-first-search on a six-by-six game board.
+    * It is *not* recommended this is ran, unlesss you have a lot of time.
+    * Contributed to by: Ricky Dodd (220010849)
+'''
+
+from DFS_Implementation import *
+
+perform_dfs()
