ac6093b2f9d6ec7d1bef8d0036c3df8b558c39d3
rickydodd-deakin <80746151+rickydodd-deakin@users.noreply.github.com>
2021-04-19 17:40:00 +1000

    Create Warnsdorff_Implementation.py
    Set out the Warnsdorff's Rule implementation file, with appropriate commenting at the top.

---
+++ Warnsdorff_Implementation.py
@@ -0,0 +1,6 @@
+'''
+    * Warnsdorff's Rule implementation to solve A Knight's Tour problem.
+    * Initial state: knight occupies, and has only come in contact, with one tile (the starting tile).
+    * Goal state: all tiles have been occupied once and only once.
+    * Contributed to by: 
+'''
